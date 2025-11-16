8423e918136af9f5fee4ab5f05ca5925d2be9003
Walter Ruiz <walter_ruiz29@hotmail.es>
2025-11-16 15:39:25 -0600

    Agregando funciones cargar usuarios y guardar usuarios

--- gestor-canciones/src/main/java/Gestion/Gestorusuarios.java
+++ gestor-canciones/src/main/java/Gestion/Gestorusuarios.java
@@ -11,3 +11,19 @@ public class Gestorusuarios {
     private static final String FILE_PATH = "usuarios.json";
     private static final Gson gson = new Gson();
 }
+
+ public static List<Usuario> cargarUsuarios() {
+        try (Reader reader = new FileReader(FILE_PATH)) {
+            return gson.fromJson(reader, new TypeToken<List<Usuario>>(){}.getType());
+        } catch (IOException e) {
+            return new ArrayList<>();
+        }
+    }
+
+    public static void guardarUsuarios(List<Usuario> usuarios) {
+        try (Writer writer = new FileWriter(FILE_PATH)) {
+            gson.toJson(usuarios, writer);
+        } catch (IOException e) {
+            System.out.println("Error guardando usuarios: " + e.getMessage());
+        }
+    }
