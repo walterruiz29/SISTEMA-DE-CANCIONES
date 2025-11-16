cbc875ba9be9d7e659bc828d87a541d8c93fdf35
Walter Ruiz <walter_ruiz29@hotmail.es>
2025-11-16 14:46:11 -0600

    mi primer commit

---
+++ gestor-canciones/src/main/java/com/mycompany/gestor/canciones/GestorCanciones.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gestor.canciones;
+
+/**
+ *
+ * @author Usuario
+ */
+public class GestorCanciones {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
